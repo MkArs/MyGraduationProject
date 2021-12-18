e92173936a47b3cde26648d6270127818d427f5b
MkArs <arsen_mkhitaryan_2000@mail.ru>
2021-12-18 11:33:55 +0300

    Added movement, shooting and picking up items

---
+++ The Binding of Isaac clone/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private float _lastTear;
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private Text _collectedText;
+    [SerializeField]
+    private GameObject _tearPrefab;
+    [SerializeField]
+    private float _shotSpeed;   
+    [SerializeField]
+    private float _tearDelay;
+    [SerializeField]
+    private float _range;
+
+    private int _collectedAmount = 0;
+
+    public int CollectedAmount { get => _collectedAmount; set => _collectedAmount = value; }
+    public float Range { get => _range; set => _range = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        float shootHorizontal = Input.GetAxis("ShootHorizontal");
+        float shootVertical = Input.GetAxis("ShootVertical");
+        if((shootHorizontal != 0f || shootVertical != 0f) && Time.time > _lastTear + _tearDelay)
+        {
+            Shoot(shootHorizontal, shootVertical);
+            _lastTear = Time.time;
+        }
+
+        _rigidbody.velocity = new Vector3(horizontal * _speed, vertical * _speed, 0f);
+        _collectedText.text = "Items collected: " + CollectedAmount;
+    }
+
+    private void Shoot(float x, float y)
+    {
+        GameObject tear = Instantiate(_tearPrefab, transform.position, transform.rotation) as GameObject;
+        tear.AddComponent<Rigidbody2D>().gravityScale = 0f;
+        tear.GetComponent<Rigidbody2D>().velocity = new Vector2(
+            (x < 0) ? Mathf.Floor(x) * _shotSpeed : Mathf.Ceil(x) * _shotSpeed,
+            (y < 0) ? Mathf.Floor(y) * _shotSpeed : Mathf.Ceil(y) * _shotSpeed    
+        );
+    }
+}
---
+++ The Binding of Isaac clone/Assets/Scripts/TearController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TearController : MonoBehaviour
+{
+    private GameObject _player;
+    private float _travelDistance = 0f;
+    private Vector2 _startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.Find("Player");
+        _startPosition = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _travelDistance += Vector2.Distance(_startPosition, gameObject.transform.position);
+        _startPosition = gameObject.transform.position;
+
+        if(_travelDistance >= _player.gameObject.GetComponent<PlayerController>().Range)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
